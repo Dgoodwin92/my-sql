ef168173ca5ccb82620eb440d2479763ddf5727a
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-05-21 21:16:44 +0000

    Added and removed lists and tables

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -11,9 +11,9 @@ connection = pymysql.connect(host='localhost',
 
 try:
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ['fred', 'Fred']
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in({});".format(format_strings), list_of_names)
+        connection.commit()
 finally:
     connection.close()
